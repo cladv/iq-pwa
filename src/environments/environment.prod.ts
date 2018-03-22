e10cb04f14dbb0eea20de6320f11b55a7ed2ce29
karenn <karenn@quintun.com>
2018-03-22 11:07:31 -0300

    Basic PWApp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyD2zWEyTY750q11orSo95cF_Fr8oTfB1Mw',
+    authDomain: 'iq-app-41bc1.firebaseapp.com',
+    databaseURL: 'https://iq-app-41bc1.firebaseio.com',
+    projectId: 'iq-app-41bc1',
+    storageBucket: 'jiq-app-41bc1.appspot.com',
+    messagingSenderId: '650066074713'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: 'AIzaSyD2zWEyTY750q11orSo95cF_Fr8oTfB1Mw',
+    authDomain: 'iq-app-41bc1.firebaseapp.com',
+    databaseURL: 'https://iq-app-41bc1.firebaseio.com',
+    projectId: 'iq-app-41bc1',
+    storageBucket: 'jiq-app-41bc1.appspot.com',
+    messagingSenderId: '650066074713'
+  }
 };
